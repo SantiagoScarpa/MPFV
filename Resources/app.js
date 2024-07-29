039975496688c7a78971fedc00ac35a4b277c894
Santiago Scarpa <santiago.scarpa@gmail.com>
2024-07-29 18:42:33 -0300

    changes

---
+++ Resources/app.js
@@ -0,0 +1,7 @@
+const btnMenu = document.querySelector('#btnNav');
+const navMenu = document.querySelector('#navbar-default')
+
+btnMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('collapse')
+    navMenu.classList.toggle('h-0')
+})
